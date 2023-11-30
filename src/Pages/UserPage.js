b718f3744f752f663100856d8794f71733edae89
Jinath Yasmin Laskar <60038325+jinath123-yasmin@users.noreply.github.com>
2023-11-30 12:21:52 +0530

    added styling in cards and posts

--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -4,8 +4,8 @@ import UserCard from '../components/UserCard';
 
 const UserPage=()=>{
 
-
   const [users, setUsers] = useState([]);
+
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -13,13 +13,15 @@ const UserPage=()=>{
       .then(users => setUsers(users))
       .catch(error => console.error('Error fetching users:', error));
   }, []);
+
+
   return(
     <div>
     <h1 className='text-center'>User Directory</h1>
     <div className="user-container">
       {users && users.map(user => (
         <Link key={user.id} to={`/user/${user.id}`}>
-          <UserCard user={user} />
+          <UserCard user={user} userId={user.id}  />
         </Link>
       ))}
     </div>
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
-function UserCard({ user }) {
-  console.log(user);
+function UserCard({ user,userId }) {
+ 
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    // Fetch user posts based on userId
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+      .then(response => response.json())
+      .then(posts => setUserPosts(posts))
+      .catch(error => console.error('Error fetching user posts:', error));
+  }, [userId]);
   return (
     <div className="user-card">
       <h3>{user.name}</h3>
-      <p>Total posts: {user.id}</p> 
+      <p>Total posts: {userPosts.length}</p> 
     </div>
   );
 }
